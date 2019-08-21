ff481666f4f96b03b3749d609b49434dd3281a19
kimseula20 <kimseula20@gmail.com>
2019-08-21 19:27:16 +0900

    dev_sa0821_signup checkId,checkEmail,checkNickname add

--- bucketclass/bucketclass/src/main/java/com/pap/bucketclass/service/SignUpService.java
+++ bucketclass/bucketclass/src/main/java/com/pap/bucketclass/service/SignUpService.java
@@ -23,43 +23,49 @@ public class SignUpService {
 	@Autowired
 	private RoleRepository roleRepo;
 
-//	// ID중복검사
-//	public Member checkId(Member member, SignUpModel signup) {
-//		System.out.println(">>>ID중복검사실시");
-//		if(signup.getMemberId().equals(member.getUsername())) {
-//			System.out.println("이미 존재하는 ID");
-//			return null;
-//		}
-//		return memberRepo.save(member);
-//	}
-//
-//	// Email 중복검사
-//	public Member checkEmail(Member member, SignUpModel signup) {
-//
-//		System.out.println(">>>Email중복검사실시");
-//		if(signup.getMemberEmail().equals(member.getMemberEmail())) {
-//			System.out.println("이미 존재하는 Email");
-//			return null;
-//		}
-//		return memberRepo.save(member);
-//	}
-//	// NickName 중복검사
-//	public Member checkNickname(Member member, SignUpModel signup) {
-//
-//		System.out.println(">>>Nickname중복검사실시");
-//		if(signup.getMemberNickname().equals(member.getMemberNickname())) {
-//			System.out.println("이미 존재하는 Nickname");
-//			return null;
-//		}
-//		return memberRepo.save(member);
-//	}
+	// ID중복검사
+	public boolean checkId(String memberId) {
+		System.out.println(">>>ID중복검사실시");
+		if(memberRepo.findById(memberId)!=null) {
+			System.out.println("이미 존재하는 ID");
+			return false;
+		}
+		return true;
+	}
+
+	// Email 중복검사
+	public boolean checkEmail(String memberEmail) {
+		System.out.println(">>>Email중복검사실시");
+		if(memberRepo.findByMemberEmail(memberEmail) != null) {
+			System.out.println("이미 존재하는 Email");
+			return false;
+		}
+		return true;
+	}
+	
+	// NickName 중복검사
+	public boolean checkNickname(String memberNickname) {
+		if(memberRepo.findByMemberNickname(memberNickname) != null) {
+			return false;
+		}
+		return true;
+	}
 
 	@Transactional
 	public Member insertMember(SignUpModel signup) {
-		Member member = signup.toMember();
-		Role role = roleRepo.findByRoleName(signup.getRoleName());
-		member.setRoles(Stream.of(role).collect(Collectors.toSet()));
-		member.setMemberIsActive(true);
-		return memberRepo.save(member);
+		try {
+			if(checkId(signup.getMemberId()) &&
+					checkEmail(signup.getMemberEmail()) &&
+					checkNickname(signup.getMemberNickname())) {
+				Member member = signup.toMember();
+				Role role = roleRepo.findByRoleName(signup.getRoleName());
+				member.setRoles(Stream.of(role).collect(Collectors.toSet()));
+				member.setMemberIsActive(true);
+				return memberRepo.save(member);
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 	}
 }
