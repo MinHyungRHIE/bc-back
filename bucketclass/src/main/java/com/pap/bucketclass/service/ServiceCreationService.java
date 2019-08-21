d339a69a13ba9bfa4e973cf35b26dd6e0cdd18c0
MinHyungRHIE <quotia72@naver.com>
2019-08-21 17:04:54 +0900

    dev_mh0821_init

---
+++ bucketclass/src/main/java/com/pap/bucketclass/service/ServiceCreationService.java
@@ -0,0 +1,20 @@
+package com.pap.bucketclass.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pap.bucketclass.entity.ServiceCreation;
+import com.pap.bucketclass.repository.ServiceCreationRepository;
+
+@Service
+public class ServiceCreationService {
+	
+	@Autowired
+	private ServiceCreationRepository serviceCreationRepo;
+	
+	public List<ServiceCreation> checkAllOfData() {
+		return serviceCreationRepo.findAll();
+	}
+}
