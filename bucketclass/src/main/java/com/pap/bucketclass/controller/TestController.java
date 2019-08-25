2e7f9e1bc671b76c49733405ac272768b478399d
MinHyungRHIE <quotia72@naver.com>
2019-08-25 21:45:54 +0900

    dev_mh0825_finallyGetCssJs

--- bucketclass/src/main/java/com/pap/bucketclass/controller/TestController.java
+++ bucketclass/src/main/java/com/pap/bucketclass/controller/TestController.java
@@ -11,4 +11,10 @@ public class TestController {
 //		System.out.println("access to Controller Get-Mapping");
 //		return "test";
 //	}
+	
+	@GetMapping("/test")
+	public String test() {
+		System.out.println("access to Controller Get-Mapping");
+		return "tester";
+	}
 }
