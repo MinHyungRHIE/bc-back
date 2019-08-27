a1ae8c2df5f9835be1fd46bbca06c6d5f464d635
kimseula20 <kimseula20@gmail.com>
2019-08-28 00:31:05 +0900

    provider&customer MyPage endPoint update

--- bucketclass/bucketclass/src/main/java/com/pap/bucketclass/controller/LoginController.java
+++ bucketclass/bucketclass/src/main/java/com/pap/bucketclass/controller/LoginController.java
@@ -1,8 +1,16 @@
 package com.pap.bucketclass.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pap.bucketclass.repository.MemberRepository;
 
 @Controller
 public class LoginController {
--- bucketclass/bucketclass/src/main/webapp/WEB-INF/views/index.jsp
+++ bucketclass/bucketclass/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib uri="http://www.springframework.org/security/tags" prefix="security" %>
     
 <!DOCTYPE html>
 <head>
@@ -19,7 +18,6 @@
 </head>
 
 <body class="transparent-header">
-<security:authorize access="hasRole('ROLE_CUSTOMER')">
 <!-- Wrapper -->
 <div id="wrapper">
 
@@ -930,7 +928,5 @@ strings: ["Attractions"," Restaurants"," Hotels"],
 		
 </div>
 <!-- Style Switcher / End -->
-</security:authorize>
-
 </body>
 </html>
